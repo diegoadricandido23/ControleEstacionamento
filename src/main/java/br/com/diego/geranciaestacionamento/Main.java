79c6a49414e37232a0a94cd17b69c5c9dd30a6fb
Diego <dcandido@ciandt.com>
2020-05-14 19:21:31 -0300

    --

--- src/main/java/br/com/diego/geranciaestacionamento/Main.java
+++ src/main/java/br/com/diego/geranciaestacionamento/Main.java
@@ -1,24 +1,11 @@
-/**
- * 
- */
 package br.com.diego.geranciaestacionamento;
 
 import br.com.diego.geranciaestacionamento.testes.ExcutaTestes;
 
-/**
- * @author dcandido
- *
- */
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		
+	public static void main(String[] args) {
 		new ExcutaTestes().iniciaTestes();
 	}
-	
-	
+
 }
